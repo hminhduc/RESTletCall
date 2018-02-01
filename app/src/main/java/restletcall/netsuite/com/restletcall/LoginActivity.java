7cc095b117929745ab6228c0dc118b69b43404d4
MinhUchiha <nqminh27894@gmail.com>
2018-02-01 10:47:43 +0700

    add logo in toolbar

--- app/src/main/java/restletcall/netsuite/com/restletcall/LoginActivity.java
+++ app/src/main/java/restletcall/netsuite/com/restletcall/LoginActivity.java
@@ -1,25 +1,55 @@
 package restletcall.netsuite.com.restletcall;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         final Button bLogin = (Button) findViewById(R.id.bLogin);
-
+        final ImageView ivLogo = (ImageView) findViewById(R.id.ivLogo);
+        SharedPreferences sharedPref = getSharedPreferences("my_data", MODE_PRIVATE);
+        Toast.makeText(LoginActivity.this, sharedPref.getString("account",""), Toast.LENGTH_SHORT).show();
+        final EditText etAccount = (EditText) findViewById(R.id.etAccount);
+        etAccount.setText(sharedPref.getString("account",""));
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                 LoginActivity.this.startActivity(intent);
+            }
+        });
+
+        ivLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i++;
+                Handler handler = new Handler();
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        i = 0;
+                    }
+                };
+                if(i == 1){
+                    handler.postDelayed(runnable, 1000);
+                }
+                if(i == 5){
+                    Intent intent = new Intent(LoginActivity.this, SettingActivity.class);
+                    LoginActivity.this.startActivity(intent);
+                }
             }
         });
     }
---
+++ app/src/main/java/restletcall/netsuite/com/restletcall/SettingActivity.java
@@ -0,0 +1,57 @@
+package restletcall.netsuite.com.restletcall;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        final EditText etAccount = (EditText) findViewById(R.id.etAccount);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etSign = (EditText) findViewById(R.id.etSign);
+        final EditText etUrl = (EditText) findViewById(R.id.etUrl);
+        final Button bSave = (Button) findViewById(R.id.bSave);
+        final Button bCancle = (Button) findViewById(R.id.bCancle);
+
+        bSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String account = etAccount.getText().toString();
+                String email = etEmail.getText().toString();
+                String sign = etSign.getText().toString();
+                String url = etUrl.getText().toString();
+
+                SharedPreferences sharedPref = getSharedPreferences("my_data", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("account", account);
+                editor.putString("email", email);
+                editor.putString("sign", sign);
+                editor.putString("url", url);
+                editor.apply();
+                Log.d("accountVal", sharedPref.getString("account",""));
+                Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+                SettingActivity.this.startActivity(intent);
+            }
+        });
+
+        bCancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+                SettingActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
